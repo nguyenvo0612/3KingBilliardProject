e49166cc625fe95adde13d0d482bc60c5d196994
quoc-int-1306 <anhquoc2002a3@gmail.com>
2024-07-20 20:04:58 +0700

    Sửa cấu trúc folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 
-import { ContentComponent } from './content/content.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { NavigationComponent } from './navigation/navigation.component';
+import { ContentComponent } from './pages/content/content.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { NavigationComponent } from './component/navigation/navigation.component';
 
 @NgModule({
   declarations: [
