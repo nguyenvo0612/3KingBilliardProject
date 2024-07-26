b1bf324f38263850859083cb275154bceadddde4
SANGTHANHNGUYEN\sangh <nguyenthanhsang.work@gmail.com>
2024-07-26 20:43:44 +0700

    add form login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './component/header/header.component';
 import { AppComponent } from './app.component';
 import { TableComponentComponent } from './pages/table.component/table.component';
+import { NavigationComponent } from './component/navigation/navigation.component';
+import {LoginComponentComponent} from './pages/login.component/login.component';
 
 const routes: Routes = [
   { path: '', component: TableComponentComponent },
   { path: 'home', component: HeaderComponent },
-  { path: '**', component: HeaderComponent },
+  { path: '', component: HeaderComponent },
+  { path: 'kk', component: NavigationComponent },
+  {path: 'login', component: LoginComponentComponent}
 ];
 
 @NgModule({
