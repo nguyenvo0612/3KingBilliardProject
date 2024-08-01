a68cf1b137e5a85c568b83112b6f42d14b3538aa
SANGTHANHNGUYEN\sangh <nguyenthanhsang.work@gmail.com>
2024-08-01 18:08:24 +0700

    add frame

---
+++ frontend/src/app/pages/frame/frame.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './frame.html',
+  styleUrls: ['./frame.css']
+})
+export class FrameComponent {
+  title = 'Billirard';
+}
